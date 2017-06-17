99c106394595411ec4f52f1f559a1844918d9e00
Damian <ja@me.com>
2017-06-17 17:07:00 +0200

    mapowanie kontrolera dla adminów na /admin

--- initial/src/main/java/com/koszulki/Controllers/AdminControllers/AdminTshirtController.java
+++ initial/src/main/java/com/koszulki/Controllers/AdminControllers/AdminTshirtController.java
@@ -18,19 +18,20 @@ import java.io.IOException;
  * Created by Damian on 17.06.2017.
  */
 @Controller
+@RequestMapping("/admin")
 public class AdminTshirtController {
 
     @Autowired
     KoszulkiService koszulkiService;
 
-    @RequestMapping(value = "/admin/manage-koszulka", method = RequestMethod.GET) //paging
+    @RequestMapping(value = "/manage-koszulka", method = RequestMethod.GET) //paging
     public String getManageKoszulki(Model uiModel)
     {
         Page<Koszulka> page = koszulkiService.getAllKoszulki(0);
         uiModel.addAttribute("page", page);
         return "admin/manage-koszulka";
     }
-    @RequestMapping(value = "/admin/manage-koszulka1", method = RequestMethod.GET) //paging
+    @RequestMapping(value = "/manage-koszulka1", method = RequestMethod.GET) //paging
     public String getManageKoszulki(Model uiModel, @RequestParam Integer pagei)
     {
         Page<Koszulka> page = koszulkiService.getAllKoszulki(pagei);
@@ -38,14 +39,14 @@ public class AdminTshirtController {
         return "admin/manage-koszulka";
     }
 
-    @RequestMapping(value = "/admin/addkoszulka", method = RequestMethod.POST)
+    @RequestMapping(value = "/addkoszulka", method = RequestMethod.POST)
     public ModelAndView addKoszulki(Model uiModel, @RequestParam String name, @RequestParam MultipartFile picture, @RequestParam Integer size) throws IOException {
         Koszulka koszulka= new Koszulka();
         koszulka.setName(name); koszulka.setPicture(picture.getBytes()); koszulka.setSize(size);
         koszulkiService.addKoszulka(koszulka);
         return new ModelAndView("redirect:/admin/manage-koszulka");
     }
-    @RequestMapping(value = "/admin/removekoszulka", method = RequestMethod.POST)
+    @RequestMapping(value = "/removekoszulka", method = RequestMethod.POST)
     public ModelAndView removeKoszulka(Model uiModel, @RequestParam Integer id)
     {
         koszulkiService.removeKoszulka(id);
