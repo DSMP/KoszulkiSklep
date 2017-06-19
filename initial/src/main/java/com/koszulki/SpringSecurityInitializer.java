06e89df5f82933f3412f077c06f6e7ee6563eac6
Damian <ja@me.com>
2017-06-19 18:49:18 +0200

    wywaliłem securityInitializer

--- initial/src/main/java/com/koszulki/SpringSecurityInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.koszulki;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * Created by Damian on 19.06.2017.
- */
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
