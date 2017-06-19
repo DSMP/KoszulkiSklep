ef380066fe44849f68d194e6e4443f9551ec37b1
Damian <ja@me.com>
2017-06-19 16:05:53 +0200

    Boże swiety aby poprawic user experience i byl jeden button do logowania i wylogowania musialem zmienic thymeleaf extras z 3.0.0 na 2.1.2. Dobrze ze jest stack overflow

---
+++ initial/src/main/java/com/koszulki/SpringSecurityInitializer.java
@@ -0,0 +1,9 @@
+package com.koszulki;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by Damian on 19.06.2017.
+ */
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
